6c867866dec4cea4b23437474890a5586a2405af
Daniela Betancourt <daniela.betancourt@sap.com>
2022-12-02 18:45:36 -0500

    folders

--- solution.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', async () => {
-    const input = await fetch('input.txt')
-    const text = await input.text()
-
-    console.log(text.replace('\r\n\r\n', ",").split(","))
-    document.querySelector(".answer1").innerHTML = JSON.stringify(input.body)
-})
