d03da5331a7b10970a474443a3cec237184a7e68
Daniela Betancourt <daniela.betancourt@sap.com>
2022-12-03 21:38:41 -0500

    change script to file

---
+++ 2022/day1/solution.js
@@ -0,0 +1,17 @@
+async function readFile() {
+    const input = await fetch('input.txt')
+    const reader = input.body.getReader()
+    const readUTF8 = await reader.read()
+    return new TextDecoder("utf-8").decode(readUTF8.value)
+}
+async function runChallenge() {
+
+    const text = await readFile()
+    const arrayTotalCalories = text.replaceAll('\r\n', '\n').replaceAll('\n\n', ",").split(",")
+        .map(food => food.split('\n')
+            .reduce((a, b) => parseInt(a) + parseInt(b), 0))
+
+    document.getElementById("answer1").innerHTML = Math.max(0, ...arrayTotalCalories)
+    document.getElementById("answer2").innerHTML = arrayTotalCalories.sort((a, b) => b - a)
+
+}
